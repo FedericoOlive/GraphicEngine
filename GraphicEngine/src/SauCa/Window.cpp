ddd793a3f82440e09c25ee8c7af9013a1bb295b5
Francisco Emilio Cavenaghi <franchucave@hotmail.com>
2022-08-17 12:43:26 -0300

    Add window class

--- GraphicEngine/src/SauCa/Window.cpp
+++ GraphicEngine/src/SauCa/Window.cpp
@@ -1,1 +1,54 @@
 #include "Window.h"
+
+Window::Window()
+{
+}
+
+Window::~Window()
+{
+}
+
+int Window::InitLibrary()
+{
+    if (!glfwInit())
+        return -1;
+}
+
+void Window::CreateWindow()
+{
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+}
+
+int Window::CheckWindow(GLFWwindow* window)
+{
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+}
+
+void Window::AssignContext(GLFWwindow* window)
+{
+    glfwMakeContextCurrent(window);
+}
+
+bool Window::WindowShouldClose(GLFWwindow* window)
+{
+    return glfwWindowShouldClose(window);
+}
+
+void Window::TerminateLibrary()
+{
+    glfwTerminate();
+}
+
+void Window::PollEvents()
+{
+    glfwPollEvents();
+}
+
+GLFWwindow* Window::GetWindow()
+{
+    return window;
+}
--- GraphicEngine/src/SauCa/Window.h
+++ GraphicEngine/src/SauCa/Window.h
@@ -1,5 +1,21 @@
 #pragma once
-class Window
+#include "Exports.h"
+
+class SAUCA_API Window
 {
+	private:
+		GLFWwindow* window;
+	public:
+		Window();
+		~Window();
+		int InitLibrary();
+		void CreateWindow();
+		int CheckWindow(GLFWwindow* window);
+		void AssignContext(GLFWwindow* window);
+		bool WindowShouldClose(GLFWwindow* window);
+		void TerminateLibrary();
+		void PollEvents();
+		GLFWwindow* GetWindow();
+
 };
 
