115a1665d98fcd31ecc26c148c4fc0c53cecff15
Sauron <federicoolive@gmail.com>
2022-08-17 11:12:01 -0300

    Add all classes by Engine Class Diagram

--- GraphicEngine/src/SauCa/BaseGame.h
+++ GraphicEngine/src/SauCa/BaseGame.h
@@ -1,10 +1,21 @@
 #pragma once
 #include "Exports.h"
 #include <iostream>
+
+#include "Input.h"
+#include "CollisionManager.h"
+#include "Renderer.h"
+#include "Window.h"
 using namespace std;
 
 class SAUCA_API BaseGame
 {
+private:
+	Input* input;
+	Window* window;
+	Renderer* renderer;
+	CollisionManager* collisionManager;
 public:
 	int Init();
+	
 };
---
+++ GraphicEngine/src/SauCa/Input.h
@@ -0,0 +1,7 @@
+#pragma once
+#include "Exports.h"
+
+class SAUCA_API Input
+{
+	
+};
