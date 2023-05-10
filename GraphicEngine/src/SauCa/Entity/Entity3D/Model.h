d0b33fcba2a9541528333b6beef9a88f9967e2cd
Sauron <federicoolive@gmail.com>
2023-05-10 20:16:21 -0300

    Add Model and Mesh class

--- GraphicEngine/src/SauCa/Entity/Entity3D/Model.h
+++ GraphicEngine/src/SauCa/Entity/Entity3D/Model.h
@@ -1,12 +1,73 @@
 #ifndef MODEL_H
 #define MODEL_H
 
-#include "Exports.h"
+#include "Entity/Entity2D/stbi.h"
+#include <assimp/scene.h>
 
-class SAUCA_API Model
+#include <string>
+#include <vector>
+
+#include "Mesh.h"
+#include "Entity/Entity3D.h"
+
+unsigned int TextureFromFile(const char* path, const std::string& directory, bool gamma = false);
+
+class SAUCA_API Model : Entity3D
 {
 public:
-	Model();
+	std::vector<MeshTexture> textures_loaded;	// stores all the textures loaded so far, optimization to make sure textures aren't loaded more than once.
+	std::vector<Mesh>    meshes;
+    std::string directory;
+    bool gammaCorrection;
+
+    Model(std::string const& path);
+	void Draw(Shader& shader);
+
+private:
+	void loadModel(std::string const& path);
+    // processes a node in a recursive fashion. Processes each individual mesh located at the node and repeats this process on its children nodes (if any).
+	void processNode(aiNode* node, const aiScene* scene);
+	Mesh processMesh(aiMesh* mesh, const aiScene* scene);
+	std::vector<MeshTexture> loadMaterialTextures(aiMaterial* mat, aiTextureType type, std::string typeName);
 };
 
+unsigned int TextureFromFile(const char* path, const std::string& directory, bool gamma)
+{
+    std::string filename = std::string(path);
+    filename = directory + '/' + filename;
+
+    unsigned int textureID;
+    glGenTextures(1, &textureID);
+
+    int width, height, nrComponents;
+    unsigned char* data = stbi_load(filename.c_str(), &width, &height, &nrComponents, 0);
+    if (data)
+    {
+        GLenum format;
+        if (nrComponents == 1)
+            format = GL_RED;
+        else if (nrComponents == 3)
+            format = GL_RGB;
+        else if (nrComponents == 4)
+            format = GL_RGBA;
+
+        glBindTexture(GL_TEXTURE_2D, textureID);
+        glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+        stbi_image_free(data);
+    }
+    else
+    {
+        std::cout << "Texture failed to load at path: " << path << std::endl;
+        stbi_image_free(data);
+    }
+
+    return textureID;
+}
 #endif
